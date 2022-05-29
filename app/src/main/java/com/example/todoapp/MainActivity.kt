0c488027ec88c1c89c423d069011b3330fb21eb9
3xt1ncion <meln1kbtw@gmail.com>
2022-05-29 17:30:44 +0300

    2

--- app/src/main/java/com/example/todoapp/MainActivity.kt
+++ app/src/main/java/com/example/todoapp/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.todoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.todoapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        val validator = Validator()
+
+        binding.button.setOnClickListener {
+
+            binding.layoutEmail.error =
+                validator.emailValid(binding.emailEditText.text)
+            binding.layoutPassword.error =
+                validator.passwordValid(binding.passwordEditText.text)
+
+            if (binding.layoutEmail.error == null && binding.layoutPassword.error == null)
+                toastShow(getString(R.string.success))
+        }
+        setContentView(binding.root)
     }
+    fun toastShow(success: String) =
+        Toast.makeText(applicationContext, success, Toast.LENGTH_SHORT).show()
 }
